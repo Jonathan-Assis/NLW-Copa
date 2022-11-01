6dec8e1787e0f3a0f9da27beecfb1f283cbfcdc6
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-11-01 00:40:15 -0300

    Add log to listen prisma query actions and find pool
    Find pools where starts with letter 'J'

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,26 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+    log: ['query'],
+})
 
 async function bootstrap() {
     const fastify = Fastify({
         logger: true,
     })
 
-    fastify.get('/pools/count', () => {
-        return { count: 0 }
+    fastify.get('/pools/count', async () => {
+        const pool = await prisma.pool.findMany({
+            where: {
+                code: {
+                    startsWith: 'J'
+                }
+            }
+        })
+
+
+        return { pool }
     })
 
     await fastify.listen({ port: 3333 })
