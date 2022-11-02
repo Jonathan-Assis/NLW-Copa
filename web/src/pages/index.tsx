e32025b9144a4f81bb0d9d1486cca29212aa02f0
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-11-02 14:20:41 -0300

    Styled Landing Page complete

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,38 +3,61 @@ import * as Img from '../assets'
 
 export default function Home() {
   return (
-   <div>
+   <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 items-center gap-28">
     <main>
       <Image src={Img.logo} alt="Logo NLW Copa" />
-      <h1>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
+      <h1 className="mt-14 text-white text-5xl font-bold leading-tight">
+        Crie seu próprio bolão da copa e compartilhe entre amigos!
+      </h1>
       
-      <div>
+      <div className="mt-10 flex items-center gap-2">
         <Image src={Img.usersAvatarExample} alt="Avatares" />
 
-        <strong>
-          <span>+12.592</span> pessoas já estão usando
+        <strong className="text-gray-100 text-xl">
+          <span className="text-ignite-500">+12.592</span> pessoas já estão usando
         </strong>
       </div>
 
-      <form>
-        <input type="text" required placeholder="Qual nome do seu bolão?" />
-        <button type="submit">Criar meu bolão</button>
+      <form className="mt-10 flex gap-2">
+        <input 
+          className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm"
+          type="text" 
+          required 
+          placeholder="Qual nome do seu bolão?" 
+        />
+        <button 
+          className="bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700"
+          type="submit"
+        >
+          Criar meu bolão
+        </button>
       </form>
 
-      <p>Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀</p>
+      <p className="mt-4 text-sm text-gray-300 leading-relaxed">
+        Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀
+      </p>
 
-      <div>
-        <div>
-          <Image src={Img.iconCheck} alt="" />
-          <div>
-            <span>+2.034</span>
+      <div className="mt-10 pt-10 border-t border-gray-600 flex items-center justify-between text-gray-100">
+        <div className="flex items-center gap-6">
+          <Image 
+            src={Img.iconCheck} 
+            alt="" 
+          />
+          <div className="flex flex-col">
+            <span className="font-bold text-2xl">+2.034</span>
             <span>Bolões criados</span>
           </div>
         </div>
-        <div>
-          <Image src={Img.iconCheck} alt="" />
-          <div>
-            <span>+192.847</span>
+
+        <div className=" w-px h-14 bg-gray-600" />
+
+        <div className="flex items-center gap-6">
+          <Image 
+            src={Img.iconCheck}
+            alt="" 
+          />
+          <div className="flex flex-col">
+            <span className="font-bold text-2xl">+192.847</span>
             <span>Palpites enviados</span>
           </div>
         </div>
@@ -50,7 +73,8 @@ export default function Home() {
   )
 }
 
-/* export const getServerSideProps = async () => {
+/*
+export const getServerSideProps = async () => {
   const response = await fetch('http://localhost:3333/pools/count')
   const data = await response.json()
 
@@ -61,4 +85,4 @@ export default function Home() {
       count: data.count
     }
   }
-} */
+}*/
