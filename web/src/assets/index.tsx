ffcbf152123f755749e7efc7a4660f7366f64623
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-11-02 12:50:17 -0300

    Create a shortcut from assets

---
+++ web/src/assets/index.tsx
@@ -0,0 +1,13 @@
+import appBackground from './app-bg.png'
+import appPreview from './app-nlw-copa-preview.png'
+import logo from './logo.svg'
+import usersAvatarExample from './users-avatar-example.png'
+import iconCheck from './icon-check.svg'
+
+export{
+    appBackground,
+    appPreview,
+    logo,
+    usersAvatarExample,
+    iconCheck
+}
