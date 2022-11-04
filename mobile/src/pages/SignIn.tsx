e15e7a2d3b18bd691de280c43f28c81486e056da
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-11-03 23:17:31 -0300

    Call the custom Hook provider

--- mobile/src/pages/SignIn.tsx
+++ mobile/src/pages/SignIn.tsx
@@ -2,8 +2,13 @@ import { Center, Text, Icon} from 'native-base'
 import { Fontisto } from '@expo/vector-icons'
 import Logo from '../assets/logo.svg'
 import {Button} from '../components/Button'
+import { useAuth } from '../hooks/useAuth'
+
 
 export function SignIn(){
+    const { signIn, user } = useAuth();
+
+    console.log('Dados do Usuário: ', user)
     return(
         <Center flex={1} bgColor="gray.900" p={7}>
             <Logo width={212} height={40} />
@@ -12,6 +17,7 @@ export function SignIn(){
                 title={'ENTRAR COM GOOGLE'}
                 mt={12}
                 leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+                onPress={signIn}
             />
             <Text color="white" textAlign="center" mt={4}>
                 Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação de sua conta.
