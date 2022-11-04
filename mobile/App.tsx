d1fdfdb289f6009a9d07943305c24142424bc363
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-11-03 23:16:57 -0300

    Applying the context provider to all the application in App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,6 +2,7 @@ import { NativeBaseProvider, StatusBar } from 'native-base';
 import { THEME } from './src/styles/theme';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from '@expo-google-fonts/roboto'
 
+import { AuthContextProvider } from './src/contexts/AuthContext';
 import { Loading } from './src/components/Loading'
 import { SignIn } from './src/pages/SignIn';
 
@@ -10,17 +11,19 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
+      <AuthContextProvider>
       <StatusBar 
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
-      />
+        />
         {
           fontsLoaded ? 
           <SignIn />
           :
           <Loading />
         }
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
