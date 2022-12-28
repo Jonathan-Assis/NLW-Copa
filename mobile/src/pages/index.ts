2bd61224e4a41755e3a49782292f063063da4f99
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-12-28 19:08:21 -0300

    Add a shortcut import/export pages

---
+++ mobile/src/pages/index.ts
@@ -0,0 +1,11 @@
+import { FindPoll } from "./FindPoll";
+import { NewPoll } from "./NewPoll";
+import { Polls } from "./Polls";
+import { SignIn } from "./SignIn";
+
+export {
+    FindPoll,
+    NewPoll,
+    Polls,
+    SignIn
+}
