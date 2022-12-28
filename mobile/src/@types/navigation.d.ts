b7357bfe76de626b35754290ac294f5e724743b8
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-12-28 19:55:55 -0300

    Created navigation typing and add navigation onPress action

---
+++ mobile/src/@types/navigation.d.ts
@@ -0,0 +1,12 @@
+export declare global {
+    namespace ReactNavigation {
+        interface RootParamList {
+            NewPoll: undefined;
+            Polls: undefined;
+            FindPoll: undefined;
+            Details: {
+                id: string;
+            };
+        }
+    }
+}
