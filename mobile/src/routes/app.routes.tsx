5f97e17f583d842a3345af809affbac5256f99cb
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-12-28 19:28:52 -0300

    Customizing the navigation bar

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,13 +1,49 @@
+import { Platform } from 'react-native';
+import { useTheme } from 'native-base';
+import { PlusCircle, SoccerBall } from 'phosphor-react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import * as Pages from '../pages'
+
+import * as Pages from '../pages';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppRoutes() {
+    const { colors, sizes } = useTheme()
+    const size = sizes[6]
+
     return (
-        <Navigator>
-            <Screen name="NewPoll" component={Pages.NewPoll} />
-            <Screen name="Polls" component={Pages.Polls} />
+        <Navigator screenOptions={{
+            headerShown: false,
+            tabBarLabelPosition: 'beside-icon',
+            tabBarActiveTintColor: colors.yellow[500],
+            tabBarInactiveTintColor: colors.gray[300],
+            tabBarStyle: {
+                position: 'absolute',
+                height: sizes[22],
+                borderTopWidth: 0,
+                backgroundColor: colors.gray[800]
+            },
+            tabBarItemStyle: {
+                position: 'relative',
+                top: Platform.OS === 'android' ? -10 : 0
+            }
+        }}>
+            <Screen
+                name="NewPoll"
+                component={Pages.NewPoll}
+                options={{
+                    tabBarIcon: ({ color }) => <PlusCircle color={color} size={size} />,
+                    tabBarLabel: 'Novo bolão'
+                }}
+            />
+            <Screen
+                name="Polls"
+                component={Pages.Polls}
+                options={{
+                    tabBarIcon: ({ color }) => <SoccerBall color={color} size={size} />,
+                    tabBarLabel: 'Meus bolões'
+                }}
+            />
         </Navigator>
     )
 }
