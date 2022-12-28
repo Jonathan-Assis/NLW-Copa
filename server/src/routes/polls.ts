0103687dd8855f701341c5da268e8d22d9d8e6de
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-12-27 22:28:31 -0300

    Create a joing access for poll

--- server/src/routes/polls.ts
+++ server/src/routes/polls.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify"
 import { prisma } from "../lib/prisma"
 import ShortUniqueId from 'short-unique-id';
 import { z } from 'zod';
+import { authenticate } from "../plugins/authenticate";
 
 export async function pollRoutes(fastify: FastifyInstance){
     fastify.get('/polls/count', async () => {
@@ -10,7 +11,7 @@ export async function pollRoutes(fastify: FastifyInstance){
         return { count }
     })
 
-    fastify.post('/polls', async (request, response) => {
+    fastify.post('/polls', async (request, reply) => {
         const createPollBody = z.object({
             title: z.string()
         })
@@ -43,9 +44,63 @@ export async function pollRoutes(fastify: FastifyInstance){
                     code
                 }
             })
-
         }
 
-        return response.status(201).send({code})
+        return reply.status(201).send({code})
+    })
+
+    fastify.post('/polls/:id/join', {
+        onRequest: [authenticate]
+    }, async (request, reply) => {
+        const joinPollBody = z.object({
+            code: z.string(),
+        })
+
+        const { code } = joinPollBody.parse(request.body)
+
+        const poll = await prisma.poll.findUnique({
+            where: {
+                code
+            },
+            include: {
+                participants: {
+                    where: {
+                        userId: request.user.sub
+                    }
+                }
+            }
+        })
+
+        if (!poll) {
+            return reply.status(400).send({
+                message: 'Poll not found.'
+            })
+        }
+
+        if (poll.participants.length > 0) {
+            return reply.status(400).send({
+                message: 'You already joined this poll.'
+            })
+        }
+
+        if(!poll.ownerId) {
+            await prisma.poll.update({
+                where: {
+                    id: poll.id,
+                },
+                data: {
+                    ownerId: request.user.sub
+                }
+            })
+        }
+
+        await prisma.participant.create({
+            data: {
+                pollId: poll.id,
+                userId: request.user.sub
+            }
+        })
+
+        return reply.status(201).send()
     })
 }
