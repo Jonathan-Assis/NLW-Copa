ea194e66b14654d66df097fce08e942e25a6c87e
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-12-28 20:36:15 -0300

    Add spinner if the user tried to access the account

--- mobile/src/pages/SignIn.tsx
+++ mobile/src/pages/SignIn.tsx
@@ -6,7 +6,7 @@ import { useAuth } from '../hooks/useAuth'
 
 
 export function SignIn(){
-    const { signIn, user } = useAuth();
+    const { signIn, isUserLoading } = useAuth();
 
     return(
         <Center flex={1} bgColor="gray.900" p={7}>
@@ -17,6 +17,8 @@ export function SignIn(){
                 mt={12}
                 leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
                 onPress={signIn}
+                isLoading={isUserLoading}
+                _loading={{ _spinner: { color: '#FFFFFF' }}}
             />
             <Text color="white" textAlign="center" mt={4}>
                 Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação de sua conta.
