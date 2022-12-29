d13b1dea208bc88d2bff6cd18815c0d7930799d3
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-12-29 17:06:49 -0300

    Add Details page to the shortcut

--- mobile/src/pages/index.ts
+++ mobile/src/pages/index.ts
@@ -1,10 +1,12 @@
 import { FindPoll } from "./FindPoll";
+import { Details } from "./Details";
 import { NewPoll } from "./NewPoll";
 import { Polls } from "./Polls";
 import { SignIn } from "./SignIn";
 
 export {
     FindPoll,
+    Details,
     NewPoll,
     Polls,
     SignIn
