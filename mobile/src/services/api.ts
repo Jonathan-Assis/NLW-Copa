739c9eae17ac5c9352cdbc316bb05a6ba0e4e360
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-12-29 17:18:45 -0300

    Config the environment variables

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// OBS: a baseURL deverá utilizar o serviço onde está rodando o mobile, no caso atual está usando o localhost/ip da maquina
+// OBS: a baseURL deverá utilizar o serviço onde está rodando o mobile, no caso atual está usando o localhost/ip da maquina onde o expo está executando + a porta do server
 export const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: process.env.HOST
 });
